9bf645d05cc3c997987e4b3acf827dfd3dc29f37
sahas26 <sahaskulasekera@gmail.com>
2021-03-29 23:45:41 +0530

    Fungal_Fact_Info

---
+++ fungal_knowledge/build/app/generated/source/buildConfig/debug/com/example/fungal_knowledge/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.fungal_knowledge;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.fungal_knowledge";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
